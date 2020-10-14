e0cf1712d91e854c716b0ddb6519092a0cdd00c5
oliveracamila <72769507+oliveracamila@users.noreply.github.com>
2020-10-13 23:45:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 let prop = document.querySelector('#propagandas');
 let cabecera = document.querySelector('#cabecera');
 let botonCerrar = document.querySelector('#button-close');
-let yaseactivo = false;
+let yaseactivo = localStorage.getItem("seActivo");
+
 
 window.addEventListener('scroll',()=>{
-    if ( (!yaseactivo) && (window.scrollY > cabecera.scrollHeight) ) {
-        prop.classList.add('prop-activas');
-        yaseactivo = true;
+    if (yaseactivo == null) {
+        if (window.scrollY > cabecera.scrollHeight) {
+            prop.classList.add('prop-activas');
+            localStorage.setItem("seActivo",true);
+            yaseactivo = true;
+        }
     }
 });
+
+
 botonCerrar.addEventListener('click',()=>{
     prop.classList.remove('prop-activas');
 })
